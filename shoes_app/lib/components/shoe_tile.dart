4393217a99d77ab4e59c55bf37f52fd025e6fba1
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-09-16 21:00:29 +0700

    Create shoe model, build shop ui

---
+++ shoes_app/lib/components/shoe_tile.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_app/models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  // Use shoe model
+  Shoe shoe;
+
+  ShoeTile({super.key, required this.shoe});
+  // const ShoeTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 20, right: 20),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Shoe picture
+          ClipRRect(
+            child: Image.asset(shoe.imagePath),
+            borderRadius: BorderRadius.circular(12),
+          ),
+
+          // Description
+          Text(shoe.description, style: TextStyle(color: Colors.grey[600])),
+
+          // Price, details
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    // Name
+                    Text(
+                      shoe.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+
+                    // Price
+                    Text(
+                      shoe.price,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+
+                // Plus button
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+
+          // Button to add to cart
+        ],
+      ),
+    );
+  }
+}
--- shoes_app/lib/pages/shop_page.dart
+++ shoes_app/lib/pages/shop_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_app/components/shoe_tile.dart';
+import 'package:shoes_app/models/shoe.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,6 +12,83 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Shopppppp", style: TextStyle(fontSize: 24)));
+    return Column(
+      children: [
+        // Search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text("Search", style: TextStyle(color: Colors.grey)),
+              Icon(Icons.search, color: Colors.grey),
+            ],
+          ),
+        ),
+
+        // Message
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            "Everyone flies, some fly longer than others",
+            style: TextStyle(color: Colors.grey[600], fontSize: 9),
+          ),
+        ),
+
+        // Hot picks
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                "Hot Picks",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                "See all",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        // Shoes
+        Expanded(
+          child: ListView.builder(
+            itemCount: 4,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              // Create new shoe
+              Shoe shoe = Shoe(
+                name: 'Air Jordan 3',
+                price: '6.690.000₫',
+                imagePath: 'lib/images/jordan_one.jpg',
+                description: 'Air Jordan 3 ‘Pure Money’',
+              );
+
+              return ShoeTile(shoe: shoe);
+            },
+          ),
+        ),
+
+        // Divider
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Divider(color: Colors.white),
+        ),
+      ],
+    );
   }
 }
